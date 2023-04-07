d1bbb50c56ad8b2be77caa155c8430e51412d9a9
ras1k <rasikabdullah@gmail.com>
2023-04-07 20:21:24 +0600

    prompts added

--- js/prompt.js
+++ js/prompt.js
@@ -1,3 +1,12 @@
 const showAlert = () =>{
     alert('lol');
 }
+const lendMoney = () =>{
+    const result = confirm('lol');
+    console.log(result)
+}
+
+const getInfo = () => {
+    const name = prompt('tell me your name');
+    console.log(name)
+}
